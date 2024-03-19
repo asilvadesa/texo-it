af2d758c2811c10ee7276723937e0cec330b4fa4
Anderson de Sa <asilvadesa@gmail.com>
2024-03-18 23:27:37 -0300

    Adição dos testes para os endpoints users e comments

--- src/test/java/com/texoit/desafio/base/BaseTest.java
+++ src/test/java/com/texoit/desafio/base/BaseTest.java
@@ -4,13 +4,12 @@ import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
 
 
-    protected static final String URI_USERS = "/users";
-    protected static final String URI_COMMENTS = "/comments";
+    protected static final String PATH_USERS = "/users";
+    protected static final String PATH_COMMENTS = "/comments";
 
     @BeforeAll
     public static void setup(){
